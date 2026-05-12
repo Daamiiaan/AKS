678592584d236442f9d42352e2a4d1e2d1b32d43
Damian <damiankuc55@gmail.com>
2026-05-12 16:17:03 +0200

    LAB1

---
+++ SchoolRegister.Model/DataModels/teacher.cs
@@ -0,0 +1,14 @@
+namespace SchoolRegister.Model.DataModels
+{
+    public class Teacher : User
+    {
+        public IList<Subject> Subjects { get; set; }
+        public string Title { get; set; }
+ 
+        public Teacher()
+        {
+            Subjects = new List<Subject>();
+        }
+    }
+}
+ 
